a6bb738153530d9e65d6dd88fb8f7214b6f842ad
Peter Collingbourne <peter@pcc.me.uk>
2010-09-22 14:57:28 +0100

    Layout for FileTransferActivity

--- src/org/connectbot/FileTransferActivity.java
+++ src/org/connectbot/FileTransferActivity.java
@@ -1,6 +1,29 @@
 package org.connectbot;
 
-import android.app.ListActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class FileTransferActivity extends ListActivity {
+public class FileTransferActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle b) {
+		super.onCreate(b);
+		setContentView(R.layout.act_filetransfer);
+
+		TabHost tabs = (TabHost) findViewById(android.R.id.tabhost);
+		tabs.setup();
+
+		TabSpec tsLocal = tabs.newTabSpec("Local");
+		tsLocal.setIndicator("Local");
+		tsLocal.setContent(R.id.localview);
+		tabs.addTab(tsLocal);
+
+		TabSpec tsRemote = tabs.newTabSpec("Remote");
+		tsRemote.setIndicator("Remote");
+		tsRemote.setContent(R.id.remoteview);
+		tabs.addTab(tsRemote);
+	}
+
 }
