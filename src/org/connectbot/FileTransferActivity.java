ba81424717c14e37ca9dc2f86b23ca7b89a8ba5f
Peter Collingbourne <peter@pcc.me.uk>
2010-10-05 00:57:31 +0100

    Remove alphabet

--- src/org/connectbot/FileTransferActivity.java
+++ src/org/connectbot/FileTransferActivity.java
@@ -40,35 +40,6 @@ public class FileTransferActivity extends Activity {
 	private ListView listLocal, listRemote;
 	private TabHost tabs = null;
 
-	private static String alphabet[] = {
-		"A",
-		"B",
-		"C",
-		"D",
-		"E",
-		"F",
-		"G",
-		"H",
-		"I",
-		"J",
-		"K",
-		"L",
-		"M",
-		"N",
-		"O",
-		"P",
-		"Q",
-		"R",
-		"S",
-		"T",
-		"U",
-		"V",
-		"W",
-		"X",
-		"Y",
-		"Z"
-	};
-
 	protected HostDatabase hostdb;
 	private HostBean host;
 
