4492caa4c5f55e2da9021a302a9ad7fb828be1ac
Peter Collingbourne <peter@pcc.me.uk>
2010-10-05 03:02:56 +0100

    Implement FileInfo.toString

--- src/org/connectbot/transport/FileInfo.java
+++ src/org/connectbot/transport/FileInfo.java
@@ -5,6 +5,10 @@ public class FileInfo implements Comparable<FileInfo> {
 	public String name;
 	public boolean isDirectory;
 
+	public String toString() {
+		return name;
+	}
+
 	public int compareTo(FileInfo other) {
 		if (isDirectory && !other.isDirectory) return -1;
 		if (!isDirectory && other.isDirectory) return 1;
